9aa40a57c9cc745ffe8eaa606c41d3aa62e6bade
Vitor Mateus Romancini do Amaral <77472862+Vitormateusromancini@users.noreply.github.com>
2023-06-09 21:28:53 -0300

    Registrador
    Exemplo do funcionamento do modo de endereçamento a registrador

---
+++ registrador.asm
@@ -0,0 +1,11 @@
+# Programa: registrador
+# Descrição: Exemplo do funcionamento do modo de endereçamento a registrador
+
+.text 
+	add $t1, $t2, $t3	# t1 <- t2 + t3
+
+	and $t4, $t2, $t3	# t4 <- t2 & t3
+	
+	slt $t5, $t2, $t3	# t5 <- 1 se t2 < t3, senão t5 <- 0
+
+	
